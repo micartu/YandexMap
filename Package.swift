541f4d0960f4ca51d82fe59563d66fb56f7d8e10
Michael Artuerhof <michael.artuerhof@gmail.com>
2024-05-20 08:16:35 +0300

    - added support for 4.6.1-lite

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "YandexMapsMobile",
-            url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.1.0-lite.framework.zip",
-            checksum: "28310d0c2661625b8137e41b2327f751c3a25d8aa389e6772990a73e39e6485c"
+            url: "https://maps-ios-pods-public.s3.yandex.net/YandexMapsMobile-4.6.1-lite.framework.zip",
+            checksum: "c08822c2ad1a9e67cbdaf45f81487e73f89685eeb807f192788859910d1ce1c1"
         )
     ]
 )
